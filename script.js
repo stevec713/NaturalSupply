aab51ce24ac81e7a3aebda1ae9826e0c305ceaab
Stephen <scareylps@gmail.com>
2025-06-23 16:15:06 -0400

    finished js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function displayCart() {
     let output = "";
     for (let item in itemList) {
-        output +=  item + ": $" + itemList[item].price.toFixed(2) + itemList[item].quantity + "<br>";
+        output +=  item + ": $" + itemList[item].price.toFixed(2) + "<br>";
     }
     document.querySelector("form p").innerHTML = output;
 }
@@ -14,16 +14,40 @@ function changeCart() {
     }
     else {
         itemList[this.name] = {
-            price: parseFloat(this.value),
-            quantity: 1
+            price: parseFloat(this.value)
         }
     }
     displayCart();
+    calculateTotal();
 }
+
+function calculateTotal() {
+    let subtotalElement = document.querySelector("dd:first-of-type");
+    let taxElement = document.querySelector("dd:nth-of-type(2)");
+    let shippingElement = document.querySelector("dd:nth-of-type(3)");
+    let totalElement = document.querySelector("dd:last-of-type");
+
+    total = 0;
+    subtotal = 0;
+    for (let item in itemList) {
+        subtotal += itemList[item].price;
+    }
+    total = subtotal + (subtotal * tax) + shipping;
+
+    subtotalElement.innerHTML = "$" + subtotal.toFixed(2);
+    taxElement.innerHTML = (tax * 100).toFixed(2) + "%";
+    shippingElement.innerHTML = "$" + shipping.toFixed(2);
+    totalElement.innerHTML = "$" + total.toFixed(2);
+}
+
+let subtotal = 0;
+let tax = .07;
+let shipping = 10;
+let total = 0;
 
 let itemList = {};
 
-let buttons = document.querySelectorAll("article button")
-for (let button of buttons) {
-    button.addEventListener("click", changeCart);
+let itemButtons = document.querySelectorAll("article button")
+for (let itemButton of itemButtons) {
+    itemButton.addEventListener("click", changeCart);
 }
